254a1b60430de76e10c4ce5a1eb6827ee994f916
Anmol Middha <anmolmiddha2827@gmail.com>
2017-09-08 15:09:59 +0530

    new commit

--- SplitMoney/app/src/main/java/com/middha/anmol/splitmoney/MainActivity.java
+++ SplitMoney/app/src/main/java/com/middha/anmol/splitmoney/MainActivity.java
@@ -1,11 +1,13 @@
 package com.middha.anmol.splitmoney;
 
 import android.content.Intent;
+
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
@@ -46,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+
 
         mGoogleBtn=(SignInButton) findViewById(R.id.Btn);
 
